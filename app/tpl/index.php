942d608c638d122848451806f163f1bd29589fb6
UAGOCN <61579539+UAGOCN@users.noreply.github.com>
2021-01-07 23:29:29 +0800

    irdll
    irdll

---
+++ app/tpl/index.php
@@ -0,0 +1,20 @@
+<?php die('Access Denied');?>
+<!doctype html>
+<html lang="zh-cmn-Hans">
+<head>
+<meta charset="utf-8">
+<title>{$title}</title>
+<!-- Favicon -->
+<link rel="shortcut icon" href="/assets/img/favicon.png">
+<link href="/assets/normalize/normalize.css" crossorigin="anonymous" />
+<link href="/assets/bootstrap/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
+<link href="/assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" crossorigin="anonymous" />
+<script type="text/javascript" src="/assets/jquery/jquery.min.js" crossorigin="anonymous"></script>
+<script type="text/javascript" src="/assets/jquery/jquery.cookie.min.js" crossorigin="anonymous"></script>
+<script type="text/javascript" src="/assets/bootstrap/bootstrap.min.js" crossorigin="anonymous"></script>
+<noscript>你的浏览器没有打开JavaScript支持，无法正常使用本页面</noscript>
+</head>
+<body>
+<h1><i class="fa fa-camera-retro" aria-hidden="true"></i>{$title}</h1>
+</body>
+</html>

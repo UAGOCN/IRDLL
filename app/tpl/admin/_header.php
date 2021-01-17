fb1730a55c34555236426b81894182600731640c
UAGOCN <61579539+UAGOCN@users.noreply.github.com>
2021-01-18 02:08:35 +0800

    irdll
    irdll

---
+++ app/tpl/admin/_header.php
@@ -0,0 +1,25 @@
+<?php die('Access Denied');?>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<meta name="applicable-device" content="pc,mobile">
+
+<!-- Favicon -->
+<link rel="shortcut icon" href="{$site_url}/assets/img/favicon.png" />
+
+<!-- Normalize CSS -->
+<link href="{$site_url}/assets/normalize/normalize.css" />
+
+<!-- Bootstrap CSS -->
+<link href="{$site_url}/assets/bootstrap/bootstrap.min.css" rel="stylesheet" />
+
+<!-- Font-awesome CSS -->
+<link href="{$site_url}/assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
+
+<!-- Main CSS -->
+<link href="{$site_url}/assets/css/style.css" rel="stylesheet" />
+
+<!--[if lt IE 9]>
+<script src="{$site_url}/assets/js/html5shiv.min.js"></script>
+<script src="{$site_url}/assets/js/respond.min.js"></script>
+<![endif]-->

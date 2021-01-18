0231e67a8d15cce5b506584745e9d4ef2a89e92e
UAGOCN <61579539+UAGOCN@users.noreply.github.com>
2021-01-18 17:39:17 +0800

    irdll
    irdll

---
+++ app/tpl/admin/_footer.php
@@ -0,0 +1,17 @@
+<?php die('Access Denied');?>
+<!-- jQuery -->
+<script src="{$site_url}/assets/jquery/jquery.min.js"></script>
+<script src="{$site_url}/assets/jquery/jquery.cookie.min.js"></script>
+
+<!-- Bootstrap Core JS -->
+<script src="{$site_url}/assets/js/popper.min.js"></script>
+<script src="{$site_url}/assets/bootstrap/bootstrap.min.js"></script>
+
+<!-- Feather Icon JS -->
+<script src="{$site_url}/assets/js/feather.min.js"></script>
+
+<!-- Custom JS -->
+<script src="{$site_url}/assets/js/custom.js"></script>
+
+<!-- Jsencrypt JS -->
+<script src="{$site_url}/assets/jsencrypt/jsencrypt.min.js"></script>
---
+++ app/tpl/terms_privacy.php
@@ -0,0 +1,25 @@
+<?php die('Access Denied');?>
+<!doctype html>
+<html lang="cmn-Hans">
+<head>
+<meta charset="utf-8">
+<title>{$title}</title>
+<link rel="shortcut icon" href="{$site_url}/assets/img/favicon.png" />
+<link href="{$site_url}/assets/normalize/normalize.css" />
+<link href="{$site_url}/assets/bootstrap/bootstrap.min.css" rel="stylesheet" />
+<link href="{$site_url}/assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
+<link href="{$site_url}/assets/css/style.css" rel="stylesheet" />
+<script src="{$site_url}/assets/jquery/jquery.min.js"></script>
+<script src="{$site_url}/assets/jquery/jquery.cookie.min.js"></script>
+<script src="{$site_url}/assets/bootstrap/bootstrap.min.js"></script>
+</head>
+<noscript>你的浏览器没有打开JavaScript支持，无法正常使用本页面</noscript>
+<body>
+
+<div class="container">
+    {$section}
+
+</div>
+
+</body>
+</html>
